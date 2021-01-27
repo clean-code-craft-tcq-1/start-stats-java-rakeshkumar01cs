b348ca977abfcb1281e5247c59bb532cbaaeeff7
rakeshkumar01cs <77484261+rakeshkumar01cs@users.noreply.github.com>
2021-01-27 23:51:22 +0530

    Update StatsChecker.java

--- src/main/java/statisticker/StatsChecker.java
+++ src/main/java/statisticker/StatsChecker.java
@@ -3,6 +3,8 @@
  */
 package statisticker;
 
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author RUK3KOR
@@ -13,46 +15,51 @@ public class StatsChecker {
   /**
    * @param maxThreshold
    * @param alerters
-   * 
-   * 
-   * 
-   * 
+   *    
    */
   float maxThreshold;
   IAlerter[] alerters;
+  float maxNumber;
   
   public StatsChecker(float maxThreshold, IAlerter[] alerters) {
- this.maxThreshold=maxThreshold;
-  this.alerters=alerters;
+    this.maxThreshold=maxThreshold;
+    this.alerters=alerters;
   }
 
   /**
    * @param args
    */
-  
-  
   public static void main(String[] args) {
  
-
   }
 
   /**
-   * @param numbers
+   * @param numberList
    */
-  public void checkAndAlert(Float[] numbers) {
-    
-    for(int i=0;i<numbers.length ;i++)
+  public void checkAndAlert(List<Float> numberList) {
+    // Getting the Max number
+    maxNumber = Collections.max(numberList);
+    if(maxNumber > this.maxThreshold)
     {
-      //for those number>
-      if(this.maxThreshold > numbers[i])
+      // Alert 
+      for(int j = 0; j < this.alerters.length; j++)
+      {
+        this.alerters[j].alert();
+      }
+    }
+   
+/*
+    //Not used code, different logic
+    for(int i=0;i<numberList.size() ;i++)
+    {
+      if(numberList.get(i) > this.maxThreshold )
       {
         for(int j = 0; j < this.alerters.length; j++)
         {
           this.alerters[j].alert();
         }
       }
-  
     }
-  }
-
+*/
+ }
 }
